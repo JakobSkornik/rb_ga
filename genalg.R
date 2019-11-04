95135b3bf2a835d658261e5f0f76031f3626e54d
JakobSkornik <jakob.novopecenmv@gmail.com>
2019-11-04 17:24:15 +0100

    first commit

---
+++ genalg.R
@@ -0,0 +1,19 @@
+individual <- setRefClass("individual", fields = c(x = "numeric", y = "numeric", fitness = "numeric"))
+
+population <- function(size) {
+
+  pop <- vector("individual", 10)
+
+  for (i in 1:size) {
+
+  }
+}
+
+
+genalg <- function() {
+
+  while (FALSE) {
+
+
+  }
+}
